8e7d4c8c65af487ae2ab0ede49ff58b259c22d3f
vsparrow <khan@vsparrow.com>
2018-03-11 15:17:59 -0400

    Created superhero form with erb and sinatra

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,18 @@
 require 'sinatra/base'
+require './config/environment.rb'
 
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
-
+    # Create a route that responds to a GET request at /.
+    get '/' do
+      # Create a view with a form and render it in the GET / route.
+      erb :super_hero
+    end
+    # The form should have fields for the name of a superhero team and their motto.
+    # There should be form inputs for each of the three superhero member's name, power, and bio.
+    post '/team' do
+      erb :team
+    end
 end
